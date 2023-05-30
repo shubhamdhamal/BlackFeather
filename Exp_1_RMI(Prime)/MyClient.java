3f3eb51a51c2c7a62bedf9b9f4df3e8c17d1e246
shubhamdhamal <76194698+shubhamdhamal@users.noreply.github.com>
2023-05-30 21:38:09 +0530

    Update MyClient.java

--- Exp_1_RMI(Prime)/MyClient.java
+++ Exp_1_RMI(Prime)/MyClient.java
@@ -1,16 +1,18 @@
-import java.rmi.*;
-import java.util.*;
-public class MyClient{
+import java.rmi.*; 
+import java.util.*; 
+public class MyClient{ 
+public static void main(String args[]){ 
+try{ 
+Scanner sc=new Scanner(System.in); 
+System.out.println("Enter Number To check Prime or Not"); 
+int num=sc.nextInt(); 
+Prime stub=(Prime)Naming.lookup("rmi://localhost:5000/sonoo"); 
+if(stub.isprime(num)){ 
+ System.out.println("The Number Is Prime"); 
+} 
+else{ 
+System.out.println("The Number Is Not Prime");} 
+}catch(Exception e){System.out.println(e);} 
+} 
+} 
 
-public static void main(String args[]){
-try{
-Scanner sc=new Scanner(System.in);
-System.out.println("Enter Number To check Prime or Not");
-int num=sc.nextInt();
-Prime stub=(Prime)Naming.lookup("rmi://localhost:5000/sonoo");
-System.out.println(stub.isprime(num));
-
-}catch(Exception e){System.out.println(e);}
-}
-
-}
